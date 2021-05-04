1d8e7ff8260cc996cbb7885650e111dfd7688103
anushka singh <anushka13singh@gmail.com>
2021-05-04 20:51:26 +0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'destroy old data'
+
+Article.destroy_all
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::DcComics.hero,
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
